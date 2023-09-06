b103bfd72105471b021af036e5529c22ce34888a
Alexsander Sampaio Lima <alexsandersampaiolima12@gmail.com>
2023-09-06 18:48:33 -0300

    feat/implementing add todo in todo form

--- src/components/components/TodoForm/index.jsx
+++ src/components/components/TodoForm/index.jsx
@@ -1,6 +1,12 @@
+import { useContext } from "react"
+import { TodoContext } from "../../../providers/TodoContext"
+
 export const TodoForm = () => {
+
+    const { addTodo } = useContext(TodoContext)
+
     return (
-        <form action="">
+        <form>
             <input type="text" />
             <textarea></textarea>
             <button type="submit">Cadastrar</button>
