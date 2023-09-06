918bc314be040975b1f9675fae1ef36f3d5a974d
Alexsander Sampaio Lima <alexsandersampaiolima12@gmail.com>
2023-09-06 17:43:13 -0300

    feat: base components todoForm, todoList, todoCard

---
+++ src/components/components/TodoForm/index.jsx
@@ -0,0 +1,9 @@
+export const TodoForm = () => {
+    return (
+        <form action="">
+            <input type="text" />
+            <textarea></textarea>
+            <button type="submit">Cadastrar</button>
+        </form>
+    )
+}
