14b1c55e6b933620c2e5e93c5b7fa832308693fe
Alexsander Sampaio Lima <alexsandersampaiolima12@gmail.com>
2023-09-06 18:13:54 -0300

    feat/creating add todo function in a todo context

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,6 +2,8 @@ import { TodoForm } from "../../components/components/TodoForm"
 import { TodoList } from "../../components/components/TodoList"
 
 export const HomePage = () => {
+    
+    
     return (
         <main>
             <TodoForm />
